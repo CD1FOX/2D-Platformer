3a05b2c9beb8cac7c3f39f4ad292d9c664619abd
Francis Leo <drypasta34@gmail.com>
2025-06-06 22:38:50 +0800

    Update
    - player movement

--- Scenes/Entity/Scripts/player.gd
+++ Scenes/Entity/Scripts/player.gd
@@ -2,7 +2,12 @@ extends CharacterBody2D
 
 var speed = 300
 var gravity_force = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#Jumping Logic
 var jump_power = -400
+var max_jump = 2
+var jump_left = 2
+var was_on_floor = false
 
 func player_movement():
 	var direction = 0
@@ -19,11 +24,19 @@ func gravity_logic(delta):
 		velocity.y += gravity_force * delta
 		
 func jump_logic():
-	if Input.is_action_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and jump_left > 0:
 		velocity.y = jump_power
+		jump_left -= 1
 
 func _physics_process(delta: float) -> void:
 	player_movement()
 	gravity_logic(delta)
 	jump_logic()
+	
+	if is_on_floor() and not was_on_floor:
+		jump_left = max_jump
+	
+	was_on_floor = is_on_floor()
+	
+	print(jump_left)
 	move_and_slide()
