66d59b9048f9e96ff558eee1d3a70b5384785b60
Francis Leo <drypasta34@gmail.com>
2025-06-07 10:30:25 +0800

    Update
    Added player animations

--- Scenes/Entity/Scripts/player.gd
+++ Scenes/Entity/Scripts/player.gd
@@ -1,30 +1,46 @@
 extends CharacterBody2D
 
-var speed = 300
+var speed = 220
 var gravity_force = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite = $AnimatedSprite2D
 
 #Jumping Logic
-var jump_power = -400
+var jump_power = -320
 var max_jump = 2
 var jump_left = 2
 var was_on_floor = false
 
 func player_movement():
 	var direction = 0
-	
+
 	if Input.is_action_pressed("Left"):
 		direction = -1
-	if Input.is_action_pressed("Right"):
+		animated_sprite.flip_h = true
+	elif Input.is_action_pressed("Right"):
 		direction = 1
-		
+		animated_sprite.flip_h = false
+
 	velocity.x = direction * speed
+
+	# Animation logic
+	if direction != 0:
+		animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Idle")
+
 
 func gravity_logic(delta):
 	if not is_on_floor():
+		if velocity.y < 0:
+			animated_sprite.play("Jump")
+		elif velocity.y > 0:
+			animated_sprite.play("Fall")
 		velocity.y += gravity_force * delta
+		
 		
 func jump_logic():
 	if Input.is_action_just_pressed("Jump") and jump_left > 0:
+		
 		velocity.y = jump_power
 		jump_left -= 1
 
@@ -38,5 +54,4 @@ func _physics_process(delta: float) -> void:
 	
 	was_on_floor = is_on_floor()
 	
-	print(jump_left)
 	move_and_slide()
