16ff6045976ff85953231008f51c88da8aa91cd5
Francis Leo <drypasta34@gmail.com>
2025-06-07 11:07:51 +0800

    Update
    Scoring System

--- Scenes/Collectibles/Scripts/apple.gd
+++ Scenes/Collectibles/Scripts/apple.gd
@@ -3,10 +3,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == 'Player':
-		Global.score += 1
 		$AnimatedSprite2D.play("Collected")
 		
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
+	Global.score += 1
 	queue_free()
