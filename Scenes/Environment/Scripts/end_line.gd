285381a8540814b7b659d4bce1d25f20787c32df
Francis Leo <drypasta34@gmail.com>
2025-06-07 11:05:39 +0800

    Update
    End Line

---
+++ Scenes/Environment/Scripts/end_line.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var already_stepped = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == 'Player' and not already_stepped:
+		$AnimatedSprite2D.play("End")
+		Global.running = false
+
+
+	
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == 'Player':
+		already_stepped = true
