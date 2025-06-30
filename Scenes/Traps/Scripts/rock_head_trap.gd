a8b85d7cfd9a3e5a0f6f38c6838292d44b3fa7c7
Francis Leo <drypasta34@gmail.com>
2025-07-01 06:45:35 +0800

    Update
    feat(rock_head_trap): make it loop arround from up > right > down > left > repeat
    
    refactor(rock_head_trap): make the code less repetive

---
+++ Scenes/Traps/Scripts/rock_head_trap.gd
@@ -0,0 +1,56 @@
+extends CharacterBody2D
+
+enum Direction { UP, RIGHT, DOWN, LEFT }
+var current_direction = Direction.UP
+var speed = 100
+var waiting_for_cooldown = false
+
+@onready var raycasts := [
+	$Up,
+	$Right,
+	$Down,
+	$Left
+]
+
+@onready var cooldown_timers := [
+	$Cooldown_UP,
+	$Cooldown_RIGHT,
+	$Cooldown_DOWN,
+	$Cooldown_LEFT
+]
+
+func _physics_process(_delta: float) -> void:
+	if waiting_for_cooldown:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+
+	# Directional movement
+	var directions = [
+		Vector2(0, -1),
+		Vector2(1, 0),
+		Vector2(0, 1),
+		Vector2(-1, 0)
+	]
+	velocity = directions[current_direction] * speed
+
+	# Raycast and collision check
+	var raycast = raycasts[current_direction]
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		if collider and not collider.is_in_group("Player"):
+			waiting_for_cooldown = true
+			raycast.enabled = false
+			cooldown_timers[current_direction].start()
+
+	move_and_slide()
+
+
+func _on_cooldown_timeout() -> void:
+	waiting_for_cooldown = false
+	current_direction = ((current_direction + 1) % 4) as Direction
+
+	# Re-enable all raycasts when completing the full cycle
+	if current_direction == Direction.UP:
+		for ray in raycasts:
+			ray.enabled = true
