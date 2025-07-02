a4f360c881128b53ee2c61f9fd8fb325d8616bd0
Francis Leo <drypasta34@gmail.com>
2025-07-03 07:37:00 +0800

    Update
    feat(spike): add spike trap that damages the player on touch

---
+++ Scenes/Traps/Scripts/spike_trap.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+var stepping_in_spike = false
+
+func _process(delta: float) -> void:
+	while stepping_in_spike:
+		Global.health -= 15 * delta
+		await get_tree().create_timer(0.4).timeout
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player") and not Global.player_dead:
+		stepping_in_spike = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player") and not Global.player_dead:
+		stepping_in_spike = false
